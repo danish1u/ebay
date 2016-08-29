fe0e744bb9c0032cc5ef5e72af3f9ed6f7d1637f
danish1u <danishprince786a@gmail.com>
2016-08-29 16:26:43 -0700

    Create sol-oz.js

---
+++ sol-oz.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function(){
+
+		$(".pagecontainer > table:eq(1) tr:first td:first").addClass("xdioms-background");
+		$(".xdioms-background table:eq(1)").addClass("xdioms-content");
+		//Footer
+	var d = new Date();
+	var footer = "\n\r<div class=\"xdioms-wrap\"><div id=\"org-footer\"><div id=\"org-designer\"><div id=\"silverouter\"><div class=\"custome-footer\"><div id=\"mylistingbuwhole\"><h3>Our Store<\/h3><p><strong>Quick Parts 247<\/strong><br>We make sure our customer is provided with a service which is quick and efficient.<br><br>Please feel free to <a href=\"http://contact.ebay.com.au/ws/eBayISAPI.dll?ContactUserNextGen&recipient=quickparts247\">Contact Us<\/a> via eBay message services<\/p><\/div><div id=\"mylistingbuwhole\"><h3>Useful Links<\/h3><ul><li><a href=\"http://stores.ebay.com.au/QuickParts247/About-Us.html\">About Us<\/a><\/li><li><a href=\"http://stores.ebay.com.au/QuickParts247/Payment.html\">Payment Policy<\/a><\/li><li><a href=\"http://stores.ebay.com.au/QuickParts247/Return.html\">Return Policy<\/a><\/li><li><a href=\"http://stores.ebay.com.au/QuickParts247/Shipping.html\">Shipping Policy<\/a><\/li><\/ul><\/div><div id=\"mylistingbuwhole\"><h3>Store Newsletter!<\/h3><p>Subscribe now for Store Newsletter and get latest Deals & Offers about store in your mail box<\/p><div id=\"signupnews\"><a href=\"http://my.ebay.com.au/ws/eBayISAPI.dll?AcceptSavedSeller&sellerid=quickparts247&ssPageName=STRK:MEFS:ADDSTR&rt=nc\">Sign Up<\/a><\/div><\/div><div id=\"mylistingbuwhole\"><h3>Other<\/h3><div id=\"payment\"><p>Copyright 2016 - Quick Parts 247.<br>All rights reserved.<br> <br><br><img src=\"http://www.pstnow.com/docs/pt/mysmarterbuy/store/images/payment_bg.png\"><\/p><\/div><\/div><\/div><\/div><div class=\"designby\"><p>2016 All Rights Reserved | Quick Parts 247<\/p><\/div><\/div><\/div><\/div>";
+
+		if($(".xdioms-content").length > 0) {
+			$(".xdioms-content").after(footer);
+		}
+
+
+});// JavaScript Document// JavaScript Document
