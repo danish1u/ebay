d5e6c688b0b7e041ff539aec84a2cb4621021297
danish1u <danishprince786a@gmail.com>
2016-08-29 16:40:33 -0700

    Create footer.js

---
+++ footer.js
@@ -0,0 +1,5 @@
+  vjo.darwin.tracking.rover.Rover.roverTrack();
+    (function() {
+        var _r = vjo.Registry;
+        _r.put('0', new vjo.darwin.transitional.ajax.AjaxThrobber("ajxThrobber_v4-0", "Page", 30, 30));
+    })();
