7f79b507ba09dab7ef0b529821e8a082d0733a62
danish1u <danishprince786a@gmail.com>
2016-08-29 18:42:09 -0700

    Create footer1.js

---
+++ footer1.js
@@ -0,0 +1,4 @@
+var con = getElementsByClass('pagecontainer');
+var mvobj = document.getElementById('footer'); //footer can be defined as promotion box
+var obj = getElementsByClass('stBadge');
+con[0].insertBefore(mvobj,obj[0]);
