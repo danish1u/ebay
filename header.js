863e00ff27d2ff9e9dcf874f681fe920b309ed31
danish1u <danishprince786a@gmail.com>
2016-08-30 17:14:53 -0700

    Create header.js

---
+++ header.js
@@ -0,0 +1,36 @@
+
+
+    document.writeln(" <div class=\"efusion-wrapcen\"> ");
+    document.writeln(" <div id=\"efusion-header\"> ");
+    document.writeln("      ");
+    document.writeln(" <div id=\"efusion-logo\"><a href=\"http://stores.ebay.com/The-Whole-Shebang-New-York?_rdc=1\"><img src=\"http://i61.photobucket.com/albums/h76/Nimo18/new%20one1_zpsmhcqgpik.png\" width=\"237\" height=\"60\" /></a></div> ");
+    document.writeln("  ");
+    document.writeln("  ");
+    document.writeln(" <!--STORE SEARCH //STARTS-->"); document.writeln("
+    <div id=\ "efusion-search\"> "); document.writeln("
+        <form action=\ "/Salma-Jewelry-Of-New-York/_i.html\" style=\ "display: inline;\" method=\ "get\" name=\ "Search\"> "); document.writeln("
+            <div id=\ "efusion-input\"> "); document.writeln(" <input name=\ "_nkw\" type=\ "text\" class=\ "v4sbox\" onclick=\ "this.value='';\" value=\ "Store Search\" size=\ "13\" maxlength=\ "300\" /> "); document.writeln(" </div> "); document.writeln("
+            <div id=\ "efusion-submit\"> "); document.writeln(" <input name=\ "submit\" type=\ "submit\" class=\ "btn-search\" /> "); document.writeln(" </div> "); document.writeln(" </form> "); document.writeln(" </div> "); document.writeln("
+    <!--STORE SEARCH //ENDS-->"); document.writeln(" "); document.writeln("
+    <!--STORE PAGES NAVIGATION //STARTS-->"); document.writeln("
+    <div id=\ "efusion-nav\"> "); document.writeln("
+        <ul> "); document.writeln("
+            <li><a href=\ "\">Contact Us</a></li> "); document.writeln("
+            <li><a href=\ "\">Ending Soon</a></li> "); document.writeln("
+            <li><a href=\ "\">New Arrivals</a></li> "); document.writeln("
+            <li><a href=\ "\">Add to Favorites</a></li> "); document.writeln("
+            <li><a href=\ "http://stores.ebay.com/The-Whole-Shebang-New-York?_rdc=1\">Home</a></li> "); document.writeln(" </ul> "); document.writeln(" </div> "); document.writeln("
+    <!--STORE PAGES NAVIGATION //END-->"); document.writeln(" "); document.writeln("
+    <!--STORE NAVIGATION //STARTS-->"); document.writeln("
+    <div id=\ "efusion-menu\"> "); document.writeln("
+        <div id=\ "efusion-menu-holder\"> "); document.writeln("
+            <ul id=\ "efusionmenu\"> "); document.writeln("
+                <li><a href=\ "\">BRACELETS</a></li> "); document.writeln("
+                <li><a href=\ "\">CHAINS</a></li> "); document.writeln("
+                <li><a href=\ "\">CHARMS</a></li> "); document.writeln("
+                <li><a href=\ "\">COLLECTIONS</a></li> "); document.writeln("
+                <li><a href=\ "\">EARRINGS</a></li> "); document.writeln("
+                <li><a href=\ "\">WEDDING RINGS</a></li> "); document.writeln("
+                <li class=\ "showall\"><a href=\ "\">SHOW ALL</a></li> "); document.writeln(" </ul> "); document.writeln(" </div> "); document.writeln(" </div> "); document.writeln("
+    <!--STORE NAVIGATION //ENDS-->"); document.writeln(" </div> "); document.writeln(" "); document.writeln("
+    <!--STORE BANNER //STARTS-->"); document.writeln("
