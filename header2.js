3fbf134295f64a9a3e2e9bf14bdb3facc75036f1
danish1u <danishprince786a@gmail.com>
2016-08-30 17:31:42 -0700

    Update and rename header1.js to header2.js

---
+++ header2.js
@@ -0,0 +1,39 @@
+    <!-- 
+    document.writeln(" <div class=\"efusion-wrapcen\"> "); 
+    document.writeln(" <div id=\"efusion-header\"> "); 
+    document.writeln("      "); 
+    document.writeln(" <div id=\"efusion-logo\"><a href=\"http://stores.ebay.com/Salma-Jewelry-Of-New-York\"><img src=\"http://i61.photobucket.com/albums/h76/Nimo18/new%20one1_zpsmhcqgpik.png\" width=\"237\" height=\"60\" /></a></div> "); 
+    document.writeln("  "); 
+    document.writeln("  "); 
+    document.writeln(" <!--STORE SEARCH //STARTS-->"); document.writeln("
+    <div id=\ "efusion-search\"> "); document.writeln("
+        <form action=\ "/Salma-Jewelry-Of-New-York/_i.html\" style=\ "display: inline;\" method=\ "get\" name=\ "Search\"> "); document.writeln("
+            <div id=\ "efusion-input\"> "); document.writeln(" <input name=\ "_nkw\" type=\ "text\" class=\ "v4sbox\" onclick=\ "this.value='';\" value=\ "Store Search\" size=\ "13\" maxlength=\ "300\" /> "); document.writeln(" </div> "); document.writeln("
+            <div id=\
+                "efusion-submit\"> "); document.writeln(" <input name=\ "submit\" type=\ "submit\" class=\ "btn-search\" /> "); document.writeln(" </div> "); document.writeln(" </form> "); document.writeln(" </div> "); document.writeln("
+    <!--STORE SEARCH //ENDS-->"); document.writeln(" "); document.writeln("
+    <!--STORE PAGES NAVIGATION //STARTS-->"); document.writeln("
+    <div id=\ "efusion-nav\"> "); document.writeln("
+        <ul> "); document.writeln("
+            <li><a href=\ "http://contact.ebay.com/ws/eBayISAPI.dll?ReturnUserEmail&requested=salmaqualitygold&frm=3692&iid=-1&de=off&redirect=0\">Contact Us</a></li> "); document.writeln("
+            <li><a href=\ "http://stores.ebay.com/Salma-Jewelry-Of-New-York/_i.html?rt=nc&_sid=647477702&_sop=1\">Ending Soon</a></li> "); document.writeln("
+            <li><a href=\ "http://stores.ebay.com/Salma-Jewelry-Of-New-York/_i.html?rt=nc&_sid=647477702&_sop=10\">New Arrivals</a></li> "); document.writeln("
+            <li><a href=\ "http://my.ebay.com/ws/eBayISAPI.dll?AcceptSavedSeller&sellerid=salmaqualitygold&refid=store&ssPageName=STORE:HTMLBUILDER:SIMPLEPAGE\">Add to Favorites</a></li> "); document.writeln("
+            <li><a href=\ "http://stores.ebay.com/Salma-Jewelry-Of-New-York\">Home</a></li> "); document.writeln(" </ul> "); document.writeln(" </div> "); document.writeln("
+    <!--STORE PAGES NAVIGATION //END-->"); document.writeln(" "); document.writeln("
+    <!--STORE NAVIGATION //STARTS-->"); document.writeln("
+    <div id=\ "efusion-menu\"> "); document.writeln("
+        <div id=\ "efusion-menu-holder\"> "); document.writeln("
+            <ul id=\ "efusionmenu\"> "); document.writeln("
+                <li><a href=\ "http://stores.ebay.com/Salma-Jewelry-Of-New-York_BRACELETS_W0QQfsubZ1666127012\">BRACELETS</a></li> "); document.writeln("
+                <li><a href=\ "http://stores.ebay.com/Salma-Jewelry-Of-New-York_CHAINS_W0QQfsubZ1666128012\">CHAINS</a></li> "); document.writeln("
+                <li><a href=\ "http://stores.ebay.com/Salma-Jewelry-Of-New-York_CHARMS_W0QQfsubZ1666126012\">CHARMS</a></li> "); document.writeln("
+                <li><a href=\ "http://stores.ebay.com/Salma-Jewelry-Of-New-York_COLLECTIONS_W0QQfsubZ1666293012\">COLLECTIONS</a></li> "); document.writeln("
+                <li><a href=\ "http://stores.ebay.com/Salma-Jewelry-Of-New-York_EARRINGS_W0QQfsubZ1666125012\">EARRINGS</a></li> "); document.writeln("
+                <li><a href=\ "http://stores.ebay.com/Salma-Jewelry-Of-New-York_WEDDING-RINGS_W0QQfsubZ1666123012\">WEDDING RINGS</a></li> "); document.writeln("
+                <li class=\ "showall\"><a href=\ "http://stores.ebay.com/Salma-Jewelry-Of-New-York/_i.html\">SHOW ALL</a></li> "); document.writeln(" </ul> "); document.writeln(" </div> "); document.writeln(" </div> "); document.writeln("
+    <!--STORE NAVIGATION //ENDS-->"); document.writeln(" </div> "); document.writeln(" "); document.writeln("
+    <!--STORE BANNER //STARTS-->"); document.writeln("
+    <div id=\ "banner-holder\"> "); document.writeln(" <img src=\"http://i61.photobucket.com/albums/h76/Nimo18/TheWholeShebang_zpsmuzcczlp.jpg?t=1472515651\" alt="" />"); document.writeln("
+         "); document.writeln(" "); document.writeln(" </div> "); document.writeln("
+    <!--STORE BANNER //ENDS-->"); document.writeln(" </div> "); document.writeln(" "); // --> 
